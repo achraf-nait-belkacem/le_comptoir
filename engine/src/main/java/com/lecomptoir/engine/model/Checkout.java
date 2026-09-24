5cc899522271d0bd1260bb0eed30861939f8a8db
Samba Diop Gomis <samba-diop.gomis@laplateforme.io>
2026-09-24 15:57:29 +0200

    feat: project v3

--- engine/src/main/java/com/lecomptoir/engine/model/Checkout.java
+++ engine/src/main/java/com/lecomptoir/engine/model/Checkout.java
@@ -1,5 +1,9 @@
 package com.lecomptoir.engine.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Checkout {
     private static final double DISCOUNT_THRESHOLD = 50.0;
     private static final double DISCOUNT_RATE = 0.10;
@@ -7,10 +11,33 @@ public class Checkout {
     public Receipt checkout(Cart cart) {
         double total = cart.getTotal();
 
+        total -= computeDrinksThirdFreeDiscount(cart);
+
         if (total > DISCOUNT_THRESHOLD) {
             total = total - (total * DISCOUNT_RATE);
         }
 
         return new Receipt(cart.getLines(), total);
+    }
+
+    private double computeDrinksThirdFreeDiscount(Cart cart) {
+        List<Double> drinksPrices = new ArrayList<>();
+
+        for (CartLine line : cart.getLines()) {
+            if (line.getProduct().getCategory() == ProductCategory.DRINKS) {
+                for (int i = 0; i < line.getQuantity(); i++) {
+                    drinksPrices.add(line.getProduct().getUnitPrice());
+                }
+            }
+        }
+
+        drinksPrices.sort(Collections.reverseOrder());
+
+        double discount = 0;
+        for (int i = 2; i < drinksPrices.size(); i += 3) {
+            discount += drinksPrices.get(i);
+        }
+
+        return discount;
     }
 }
